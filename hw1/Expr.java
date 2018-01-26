2e60ad0dc2b509e10e41e0dfc5312c4f945eb7b9
TylerWasniowski <TylerWasniowski@gmail.com>
2018-01-26 14:25:05 -0800

    Added required files

---
+++ hw1/Expr.java
@@ -0,0 +1,6 @@
+/**
+ * Created by rooke on 1/26/2018.
+ */
+public interface Expr<T> {
+    T value();
+}
---
+++ hw1/Op.java
@@ -0,0 +1,9 @@
+/**
+ * Created by rooke on 1/26/2018.
+ */
+public class Op<T> {
+
+    @SafeVarargs public Op(Function<T> fun, Expr<T>... args) {
+
+    }
+}
