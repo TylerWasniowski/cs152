1a0ea20dfa9024427f779b38aa664af19279abd3
TylerWasniowski <TylerWasniowski@gmail.com>
2018-01-26 15:25:55 -0800

    Finished testing

--- hw1/ExprTest2.java
+++ hw1/ExprTest2.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
+import java.io.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
-/**
- * Created by rooke on 1/26/2018.
- */
-public class ExprTest2 {
+public class ExprTest2
+{
+    private static <T> Const<T> c(T v) { return new Const<T>(v); }
+
+    @Test public void emptyExpr()
+    {
+        assertEquals(1, (Object) new Product().value());
+    }
+
+    @Test public void negativeProduct()
+    {
+        assertEquals(-30, (long) new Product(c(-5), c(6)).value());
+    }
+
+    @Test public void postiveProductNegativeArgs() {
+        assertEquals(50, (Object) new Product(c(-5), c(-5), c(-1), c(-2)).value());
+    }
 }
