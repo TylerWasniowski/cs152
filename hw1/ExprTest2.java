dc41960a3ca49c11b775653c9f0596e36fc7e1f0
TylerWasniowski <TylerWasniowski@gmail.com>
2018-01-26 21:36:33 -0800

    Changed casting from long to Object to conform to other test file

--- hw1/ExprTest2.java
+++ hw1/ExprTest2.java
@@ -13,7 +13,7 @@ public class ExprTest2
 
     @Test public void negativeProduct()
     {
-        assertEquals(-30, (long) new Product(c(-5), c(6)).value());
+        assertEquals(-30, (Object) new Product(c(-5), c(6)).value());
     }
 
     @Test public void postiveProductNegativeArgs() {
