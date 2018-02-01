6026c8cd2960b3eac36d670c6f4abeb43d52150e
TylerWasniowski <TylerWasniowski@gmail.com>
2018-01-31 22:50:36 -0800

    Finished number 2

--- hw2/hw2.scala
+++ hw2/hw2.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object hw2 {
   def subs(s: String) : String = {
     def helper(str: String, ret: String) : String = {
@@ -13,7 +15,23 @@ object hw2 {
   }
 
   def lcs(a: String, b: String) : String = {
-    ""
+    @tailrec def helper(shorterString: String, longerString: String) : String = {
+      if (longerString.contains(shorterString))
+        shorterString
+      else
+        helper(shorterString.substring(0, shorterString.length() - 1), longerString)
+    }
+
+    val shorterString = if (a.length() < b.length()) a else b
+    val longerString = if (a != shorterString) a else b
+
+    if (shorterString.isEmpty)
+      shorterString
+    else {
+      val usingFirstChar = helper(shorterString, longerString)
+      val notUsingFirstChar = lcs(shorterString.substring(1), longerString)
+      if (usingFirstChar.length() > notUsingFirstChar.length()) usingFirstChar else notUsingFirstChar
+    }
   }
 
   def onebits(n: Int) : List[Int] = {
--- hw2/hw2test.scala
+++ hw2/hw2test.scala
@@ -1,6 +1,11 @@
 object hw2test extends App {
   println(hw2.subs("Amy"))
   println(hw2.subs("Tyler"))
+  println(hw2.lcs("permission", "cruise missile"))
+  println(hw2.lcs("emissions", "mississippi"))
+  println(hw2.lcs("Yogurt", "Television"))
+  println(hw2.lcs("", ""))
+  println(hw2.lcs("Colon", "Zebra"))
   println(hw2.lcs("Mary had a little lamb", "Its fleece was white as snow"))
   // your other test cases
 }
