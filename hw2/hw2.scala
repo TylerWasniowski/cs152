97f8552ae854d3d8831013819ebca73cb82f961d
TylerWasniowski <TylerWasniowski@gmail.com>
2018-01-31 20:04:57 -0800

    Fixed subs function

--- hw2/hw2.scala
+++ hw2/hw2.scala
@@ -4,13 +4,13 @@
 
 object hw2 {
   def subs(s: String) : String = {
-    def helper(s: String, ret: String) : String = {
-      if (s.isEmpty)
+    def helper(str: String, ret: String) : String = {
+      if (str.isEmpty)
         ret
       else {
-        val notUsingFirstChar = helper(s.substring(1), ret)
-        val usingFirstChar = helper(s.substring(1), ret + s.substring(0))
-        usingFirstChar + "|" + notUsingFirstChar
+        val notUsingFirstChar = helper(str.substring(1), ret)
+        val usingFirstChar = helper(str.substring(1), ret + str.charAt(0))
+        notUsingFirstChar + "|" + usingFirstChar
       }
     }
     helper(s, "")
