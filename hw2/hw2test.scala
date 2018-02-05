97afdf979354eb32037ae1378b4cb8771ac665b7
TylerWasniowski <TylerWasniowski@gmail.com>
2018-02-05 12:01:43 -0800

    Added test case

--- hw2/hw2test.scala
+++ hw2/hw2test.scala
@@ -3,6 +3,7 @@ object hw2test extends App {
   println(hw2.subs("Tyler"))
   println(hw2.lcs("permission", "cruise missile") + "| Expected: \"missi\"")
   println(hw2.lcs("emissions", "mississippi") + "| Expected: \"missi\"")
+  println(hw2.lcs("Horstmann", "Horse") + "| Expected: \"Hors\"")
   println(hw2.lcs("Yogurt", "Television") + "| Expected: \"o\"")
   println(hw2.lcs("", "") + "| Expected: \"\"")
   println(hw2.lcs("Colon", "Zebra") + "| Expected: \"\"")
