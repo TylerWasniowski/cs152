f37f094bbd6431d051ea357b1e5bc048c34c8413
TylerWasniowski <TylerWasniowski@gmail.com>
2018-03-09 22:06:04 -0800

    Testing part1

--- hw6/src/main/scala/hw6part1.scala
+++ hw6/src/main/scala/hw6part1.scala
@@ -14,13 +14,22 @@ object hw6part1 {
     def eval: Int = {
       expr match {
         case Number(n) => n
-        case Variable(name) => Prog(defs, defs.find(_.name == name).get.value).eval
-        case Operator(left, right, f) => f(eval(left, symbols), eval(right, symbols))
+        case Variable(name) => eval(defs, defs.find(_.name == name).get.value)
+        case Operator(left, right, f) => f(eval(defs, left), eval(defs, right))
+        case definition: Def => eval(definition :: defs, expr)
       }
+    }
+
+    private def eval(defs: List[Def], expr: Expr): Int = {
+      Prog(defs, expr).eval
     }
   }
 
   class SimpleLanguageParser extends JavaTokenParsers {
+    def prog: Parser[Prog] = (rep(valdef) ~ expr) ^^ {
+      case lst ~ expr => Prog(lst, expr)
+    }
+
     def expr: Parser[Expr] = (term ~ rep(("+" | "-") ~ term)) ^^ {
       case a ~ lst => (a /: lst) {
         case (x, "+" ~ y) => Operator(x, y, _ + _)
@@ -40,11 +49,9 @@ object hw6part1 {
     def valdef: Parser[Def] = ("val" ~> ident <~ "=") ~ expr <~ ";" ^^ { case s ~ e => Def(s, e) }
   }
 
-  object Main extends App {
-    val parser = new SimpleLanguageParser
-    parser.parseAll(parser.prog, new InputStreamReader(System.in)) match {
-      case parser.Success(result, next) => println(result.eval)
-      case _ => println("Error")
-    }
+  val parser = new SimpleLanguageParser
+  parser.parseAll(parser.prog, new InputStreamReader(System.in)) match {
+    case parser.Success(result, next) => println(result.eval)
+    case _ => println("Error")
   }
 }
