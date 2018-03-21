b990500e4f893a6f854feda8c00a324ec4a68cde
TylerWasniowski <TylerWasniowski@gmail.com>
2018-03-21 12:12:20 -0700

    10 mins

--- midterm/src/main/scala/problem1.scala
+++ midterm/src/main/scala/problem1.scala
@@ -1,5 +1,14 @@
 object problem1 extends App {
-  def diffs(lst: List[Int]) = ...
+  def diffs(lst: List[Int]): List[Int] = {
+    def helper(input: List[Int], output: List[Int]): List[Int] = {
+      if (input == Nil || input.tail == Nil)
+        output
+      else
+        input.head - input.tail.head :: helper(input.tail, output)
+    }
+
+    helper(lst, Nil)
+  }
 
   println(diffs(List(3, 1, 4, 1, 5, 9, 2, 6)))
 }
