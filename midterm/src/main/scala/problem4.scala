4d8e667a6b23a3665c4a8c7a539022c7017cfdef
TylerWasniowski <TylerWasniowski@gmail.com>
2018-03-21 12:38:30 -0700

    40 mins

--- midterm/src/main/scala/problem4.scala
+++ midterm/src/main/scala/problem4.scala
@@ -1,23 +1,29 @@
 import scala.util.parsing.combinator._
 
 object problem4 extends App {
-   class SimpleLanguageParser extends JavaTokenParsers {    
-     def expr: Parser[Int] = (term ~ opt(("+" | "-") ~ expr)) ^^ { 
-       case a ~ None => a
-       case a ~ Some("+" ~ b) => a + b
-       case a ~ Some("-" ~ b) => a - b
-     } 
-    
-     def term: Parser[Int] = (factor ~ opt(("*" | "/") ~ term)) ^^ { 
-       case a ~ None => a
-       case a ~ Some("*" ~ b) => a * b
-       case a ~ Some("/" ~ b) => a / b
-     }
-     
-     def factor: Parser[Int] = wholeNumber ^^ (_.toInt) | "(" ~> expr <~ ")"
+  class SimpleLanguageParser extends JavaTokenParsers {
+    def expr: Parser[Boolean] = (term ~ rep("||" ~ term)) ^^ {
+      case term ~ list => list.foldLeft(term) {
+        case (a, "||" ~ b) => a || b
+      }
+    }
+
+    def term: Parser[Boolean] = (factor ~ rep("&&" ~ factor)) ^^ {
+      case factor ~ list => list.foldLeft(factor) {
+        case (a, "&&" ~ b) => a && b
+      }
+    }
+
+    def factor: Parser[Boolean] = (rep("!") ~ "true") ^^ {
+      case list ~ a => if (list.length % 2 == 0) true else false
+    } | (rep("!") ~ "false") ^^ {
+      case list ~ a => if (list.length % 2 != 0) true else false
+    } | "(" ~> expr <~ ")"
   }
-   
+
   val parser = new SimpleLanguageParser
   val result = parser.parseAll(parser.expr, " !true || !false && !!true")
-  println(result)         
+  println(result)
 }
+
+
