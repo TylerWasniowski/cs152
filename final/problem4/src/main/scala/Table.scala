366bce175e171d234b55b629548dcefa3e1ba1e7
TylerWasniowski <TylerWasniowski@gmail.com>
2018-05-16 10:53:52 -0700

    4 is compiling

--- final/problem4/src/main/scala/Table.scala
+++ final/problem4/src/main/scala/Table.scala
@@ -1,7 +1,11 @@
-class Table[K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
-  def get(k: K) = entries.find(x => x == k)
+class Table[-K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
+  def get[A <: K](k: A): Option[V] =
+    entries.find(x => x == k) match {
+      case Some(x) => Some(x._2)
+      case None => None
+    }
   // TODO: Implementation
   // TODO: The put method doesn't quite work. Make the appropriate
   // changes.
-  def put(k: K, v: V): Table[K, V] = new Table((k, v) :: entries)
+  def put[M >: V](k: K, v: M): Table[K, M] = new Table((k, v) :: entries)
 }
