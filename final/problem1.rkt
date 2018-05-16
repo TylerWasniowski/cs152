cda3f642bd9bac9968f248a4ad465f65afddb87c
TylerWasniowski <TylerWasniowski@gmail.com>
2018-05-16 10:18:40 -0700

    Finished problem 1

--- final/problem1.rkt
+++ final/problem1.rkt
@@ -3,5 +3,6 @@
 (define (invertCycle cycle)
   (cons (first cycle) (foldl (λ (x inverted) (cons x inverted)) '() (rest cycle))))
 
-(define (invert permutation) (* 3 3) )
+(define (invert permutation)
+  (foldr (λ (x inverted) (cons (invertCycle x) inverted)) '() permutation))
         
