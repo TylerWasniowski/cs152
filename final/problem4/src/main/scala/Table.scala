bd2f2866426df3b5543baf13359fb348ceddee62
TylerWasniowski <TylerWasniowski@gmail.com>
2018-05-16 10:42:41 -0700

    Started 2

--- final/problem4/src/main/scala/Table.scala
+++ final/problem4/src/main/scala/Table.scala
@@ -1,5 +1,6 @@
-class Table[K,V](entries: List[(K,V)] = List()) { // TODO: Type variance
-  def get(k: K) = . . . // TODO: Implementation
+class Table[K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
+  def get(k: K) = entries.find(x => x == k)
+  // TODO: Implementation
   // TODO: The put method doesn't quite work. Make the appropriate
   // changes.
   def put(k: K, v: V): Table[K, V] = new Table((k, v) :: entries)
